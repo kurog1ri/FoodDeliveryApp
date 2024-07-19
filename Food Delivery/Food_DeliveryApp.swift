d720bcbd47ca4d8c6ad25c1176afba818e1c700c
kurog1ri <kosenko.yosiki@gmail.com>
2024-07-19 13:35:29 +0300

    Initial Commit

---
+++ Food Delivery/Food_DeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_DeliveryApp.swift
+//  Food Delivery
+//
+//  Created by   Kosenko Mykola on 19.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct Food_DeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
